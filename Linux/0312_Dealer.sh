9bdcee3bc977bbcec15ee6620257a81dd59db026
TrentMarier <Trentmarermn@gmail.com>
2021-12-29 03:59:20 -0600

    First commit

---
+++ Linux/0312_Dealer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/sysadmin/Lucky_Duck_Investigations/Roulette_Loss_Investigation/Dealer_Analysis
+awk '{print $1, $2, $5, $6}' 0312_Dealer_schedule | grep $1 | grep -i $2 >> Dealers_working_during_losses
